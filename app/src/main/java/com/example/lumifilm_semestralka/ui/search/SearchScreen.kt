3dfe191fe1e0a7372dd531cc2f4243d393892e40
1Timax1 <vladtimoshevskyi@gmail.com>
2026-05-09 22:39:35 +0200

    Pridanie zaciatocnych obrazoviek s textom ze co zobrazuju, skusam

---
+++ app/src/main/java/com/example/lumifilm_semestralka/ui/search/SearchScreen.kt
@@ -0,0 +1,19 @@
+package com.example.lumifilm_semestralka.ui.search
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun SearchScreen(navController: NavController) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Hladaj si Screen")
+    }
+}
